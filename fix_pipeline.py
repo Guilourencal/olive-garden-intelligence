fe44e924e0b7a08c0e48f3e47ca8031ce5a471a8
guilherme <guilourencal@gmail.com>
2026-04-22 15:28:08 -0300

    remove arquivos temporarios

--- fix_pipeline.py
+++
@@ -1,7 +0,0 @@
-﻿lines = open('atualizar_tudo.py', 'r', encoding='utf-8').read().split('\n')
-for i, line in enumerate(lines):
-    if '("Classificando social...", "classificar_social.py"),' in line:
-        lines.insert(i, '    ("Coletando Instagram...", "coletar_instagram.py"),')
-        break
-open('atualizar_tudo.py', 'w', encoding='utf-8').write('\n'.join(lines))
-print('Feito!')
