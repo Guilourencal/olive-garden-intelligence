dc8a56a54458c47c8c832d4405caea64b85ae228
guilherme <guilourencal@gmail.com>
2026-04-22 11:46:44 -0300

    atualizacao completa dados abril 2026

---
+++ fix_lixo.py
@@ -0,0 +1,4 @@
+﻿lines = open('dashboard.py', 'r', encoding='utf-8').read().split('\n')
+result = lines[:871] + lines[878:]
+open('dashboard.py', 'w', encoding='utf-8').write('\n'.join(result))
+print('Feito! Linhas:', len(result))
